1eadb0ad4f1d965204dab696de75f7e2be6017c6
bhavya1099 <63062434+bhavya1099@users.noreply.github.com>
2024-07-30 11:10:40 +0530

    Unit test generated by RoostGPT
    Using AI Model gpt-4

---
+++ src/test/java/com/hubberspot/mockito/spies/Categories.java
@@ -0,0 +1,73 @@
+package com.hubberspot.mockito.spies;
+
+public interface Categories {
+
+	interface getBookId {
+
+	}
+
+	interface roostTestTag1 {
+
+	}
+
+	interface roostTestTag2 {
+
+	}
+
+	interface setBookId {
+
+	}
+
+	interface getTitle {
+
+	}
+
+	interface setTitle {
+
+	}
+
+	interface getPrice {
+
+	}
+
+	interface setPrice {
+
+	}
+
+	interface getPublishedDate {
+
+	}
+
+	interface setPublishedDate {
+
+	}
+
+	interface equals {
+
+	}
+
+	interface equals {
+
+	}
+
+	interface equals {
+
+	}
+
+	interface hashCode {
+
+	}
+
+	interface applyDiscountOnBook {
+
+	}
+
+	interface findBook {
+
+	}
+
+	interface getAppliedDiscount {
+
+	}
+
+}
